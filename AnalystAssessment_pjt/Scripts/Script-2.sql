dd7e759cd5d1396bb8707e0c116447b1d809b490
Byungchul_Junn <zenith9108@naver.com>
2019-05-05 16:39:17 +0900

    hi

--- AnalystAssessment_pjt/Scripts/Script-2.sql
+++
@@ -1,5 +0,0 @@
-CREATE SEQUENCE industry
-CREATE TABLE industry(
-	industryIdx number(5) PRIMARY KEY,
-	industryName varchar2(50) NOT NULL
-);
--- AnalystAssessment_pjt/Scripts/Script-4.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER springuser
-IDENTIFIED BY 0000
-DEFAULT tablespace users
-TEMPORARY TABLESPACE temp;
-GRANT resource, CONNECT TO springuser;
